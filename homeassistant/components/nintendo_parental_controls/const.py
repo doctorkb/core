61c68790f97ecabd45d5e6e013e1541e0646c029
Erik <erik@montnemery.com>
2025-10-07 09:54:38 +0200

    Change domain and name of Nintendo Switch parental controls integration

---
+++ homeassistant/components/nintendo_parental_controls/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Nintendo Switch parental controls integration."""
+
+DOMAIN = "nintendo_parental_controls"
+CONF_UPDATE_INTERVAL = "update_interval"
+CONF_SESSION_TOKEN = "session_token"
---
+++ tests/components/nintendo_parental_controls/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the Nintendo Switch parental controls integration."""
